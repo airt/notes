e6c10eaa8034ed5c750f734c47e431cc5ce9a8c9
airt <airtial@gmail.com>
2017-08-22 21:57:59 +0800

    build: upgrade theme next

--- .scripts/install-theme.sh
+++ .scripts/install-theme.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
+version='v5.1.2'
+
 if [ ! -d 'themes/next' ]
 then
-  git clone https://github.com/iissnan/hexo-theme-next themes/next
+  git clone --branch $version --depth 1 https://github.com/iissnan/hexo-theme-next themes/next
   cp themes/next-config.yml themes/next/_config.yml
 fi
