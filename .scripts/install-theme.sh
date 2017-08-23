3a43d1e38a05d65f734edeb9331b14d03f69b0ef
airt <airtial@gmail.com>
2017-08-23 14:18:58 +0800

    build: update theme next config

--- .scripts/install-theme.sh
+++ .scripts/install-theme.sh
@@ -8,5 +8,6 @@ version='v5.1.2'
 if [ ! -d 'themes/next' ]
 then
   git clone --branch $version --depth 1 https://github.com/iissnan/hexo-theme-next themes/next
+  mv themes/next/_config.yml themes/next/_config.yml.b
   cp themes/next-config.yml themes/next/_config.yml
 fi
